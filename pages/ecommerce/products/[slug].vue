7acb6d5266f5bf39af9306f0e071fc050540aa4f
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-06-16 10:34:08 -0400

    M

--- pages/ecommerce/products/[slug].vue
+++ pages/ecommerce/products/[slug].vue
@@ -1,23 +1,24 @@
 <script setup>
-definePageMeta({
-  layout: 'admin',
-})
-const pageTitle = ref('Product | YRL')
+const pageTitle = ref("Product | YRL")
 
 const config = useRuntimeConfig()
-const { errorMsg, message, galleryMedia, mediaReference, showMediaSelector } = useAppState()
+const {
+  errorMsg,
+  message,
+  galleryMedia,
+  mediaReference,
+  showMediaSelector,
+} = useAppState()
 const { fetchAll, saveMedia, saveDoc, deleteDocs } = useHttp()
 const route = useRoute()
 const router = useRouter()
 
 let response = null
-const product = useState('product', () => {
-  return { productType: 'variable', gallery: [], categories: [], attributes: [], variants: [] }
-})
-const slug = route.params.slug === '_' ? null : route.params.slug
+const product = ref({})
+const slug = route.params.slug === "_" ? null : route.params.slug
 
 if (slug) {
-  response = await fetchAll('products', { slug })
+  response = await fetchAll("products", { slug, populate: "gallery oemPartNumber" })
   if (response.docs) product.value = response.docs[0]
   console.log(response)
   // if (product.value.id) {
@@ -28,18 +29,21 @@ if (slug) {
 
 const saveProduct = async () => {
   console.log(product.value)
-  if (!product.value.name) return (errorMsg.value = 'Product name is required')
+  if (!product.value.name) return (errorMsg.value = "Product name is required")
   // const id = product.value.id ? product.value.id : null
-  const newProduct = await saveDoc('products', product.value)
-  console.log('newProduct', newProduct)
+  const newProduct = await saveDoc("products", product.value)
+  console.log("newProduct", newProduct)
   if (!Object.values(newProduct).length) return
   // slug = newProduct.slug
-  message.value = 'product saved succesfully'
+  message.value = "product saved succesfully"
   // response = await deleteDocs('variants', { product: newProduct.id })
   // if (!response) return
   // if (product.value.variants.length) response = await $saveDoc('variants', product.value.variants)
-  router.push({ name: 'admin-ecommerce-products-slug', params: { slug: newProduct.slug } })
-  message.value = 'product saved succesfully'
+  router.push({
+    name: "admin-ecommerce-products-slug",
+    params: { slug: newProduct.slug },
+  })
+  message.value = "product saved succesfully"
   // response = await $fetchBySlug('products', slug)
   // if (response) product.value = response
 }
@@ -65,80 +69,69 @@ const saveProduct = async () => {
 watch(
   () => galleryMedia.value,
   (currentVal) => {
-    if (mediaReference.value === 'productMedia') product.value.gallery[0] = currentVal[0]
+    if (mediaReference.value === "productMedia") product.value.gallery[0] = currentVal[0]
   },
   { deep: true }
 )
 </script>
 
 <template>
-  <div class="hfull flex-col items-center gap-2 p-3">
+  <div class="flex-1 flex-row justify-center items-center border-red">
     <Title>{{ pageTitle }}</Title>
-    <header class="flex-col gap-2 w-full max-width-130">
-      <div class="go-back" id="product-go-back">
-        <NuxtLink class="admin-link" :to="{ name: 'admin-ecommerce-products' }">
-          <IconsArrowWest /><span>Products</span>
-        </NuxtLink>
+    <div class="w-996p">
+      <header class="flex-col gap-2 w-full max-width-130">
+        <div class="go-back" id="product-go-back">
+          <NuxtLink class="admin-link" :to="{ name: 'admin-ecommerce-products' }">
+            <IconsArrowWest /><span>Products</span>
+          </NuxtLink>
+        </div>
+      </header>
+      <main class="flex-1 flex-col gap-2">
+        <div class="flex-row gap-2">
+          <div
+            class="image w-20 h-20 flex-row justify-center items-center overflow-hidden"
+          >
+            <img
+              class="w-full h-full cover br-3"
+              v-if="
+                product.gallery.length &&
+                product.gallery[0] &&
+                product.gallery[0].mimetype.includes('image')
+              "
+              :src="`${config.backendUrl}/${product.gallery[0].path}`"
+            />
+            <img
+              v-else
+              class="w-full h-full cover br-3"
+              :src="`${config.backendUrl}/placeholder.png`"
+            />
+          </div>
+          <div class="">
+            <div>{{ product.name }}</div>
+            <div>{{ product.oemPartNumber.name }}</div>
+            <div>{{ product.description }}</div>
+          </div>
+        </div>
+        <div class="flex-row gap-2">
+          <div>
+            <h3>Eligibility</h3>
+          </div>
+          <div>
+            <h3>Next Higher Assembly</h3>
+          </div>
+        </div>
+      </main>
+      <div class="w-full flex-row justify-end px-4 sticky bottom-4 go-to-top">
+        <a href="#product-go-back" class="btn btn__secondary px-2 py-1">Go To Top</a>
       </div>
-      <h3 class="header">Edit Product</h3>
-    </header>
-    <main class="main flex-1 max-width-130 w-full">
-      <!-- <div class="left-sidebar shadow-md">
-        <EcommerceAdminProductLeftSidebar :product="product" />
-      </div> -->
-      <div class="flex-col gap-2">
-        <!-- <EcommerceAdminProductGeneralInfo /> -->
-        <!-- <EcommerceAdminProductPrice :product="product" @updatePrice="product.value = { ...product.value, ...$event }" /> -->
-        <!-- <EcommerceAdminProductEligibility /> -->
-        <!-- <EcommerceAdminProductNextHigherAssembly /> -->
-        <!-- <EcommerceAdminProductStockManagement
-          :product="product"
-          @updateStock="product.value = { ...product.value, ...$event }"
-        /> -->
-
-        <!-- <EcommerceAdminProductAttributesContent
-          v-if="product.id && product.productType === 'variable'"
-          @toggleAttributesSlideout="showAttributesSlideout = $event"
-        /> -->
-        <!-- <EcommerceAdminProductAttributesSlideout
-          v-if="showAttributesSlideout"
-          @toggleAttributesSlideout="showAttributesSlideout = $event"
-          @saveAttributes="saveProduct"
-        /> -->
-
-        <!-- <EcommerceAdminProductVariantsContent
-          @toggleVariantsSlideout="showVariantsSlideout = $event"
-          v-if="product.id && product.productType === 'variable' && product.attributes.length"
-        /> -->
-        <!-- <EcommerceAdminProductVariantsSlideout
-          v-if="showVariantsSlideout"
-          @toggleVariantsSlideout="showVariantsSlideout = $event"
-          @saveVariants="saveProduct"
-        /> -->
-        <!-- <EcommerceProductDetails :product="product" @updateDetails="product.value = { ...product.value, ...$event }" /> -->
-
-        <!-- <EcommerceProductShippingOptions :product="product" /> -->
-        <!-- <EcommerceProductDigitalDelivery :product="product" /> -->
-        <!-- <EcommerceProductExtraFields :product="product" /> -->
-        <!-- <EcommerceProductSeo :product="product" /> -->
-        <!-- <EcommerceProductMisc :product="product" /> -->
-      </div>
-      <div class="right-sidebar">
-        <!-- <EcommerceAdminProductRightSidebar @productStatusUpdated="product.status = $event" @saveProduct="saveProduct" /> -->
-        <section class="admin-image-gallery shadow-md p-2 flex-col gap-2 bg-white" id="image-gallery">
-          <!-- <EcommerceAdminImageGallery :gallery="product.gallery" /> -->
-        </section>
-      </div>
-    </main>
-    <div class="w-full flex-row justify-end px-4 sticky bottom-4 go-to-top">
-      <a href="#product-go-back" class="btn btn__secondary px-2 py-1">Go To Top</a>
     </div>
+
     <!-- <footer class="w-full max-width-130 bg-slate-300 px-2 py-1 br-5 flex-row justify-center text-2xl">Footer</footer> -->
   </div>
 </template>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/variables';
+@import "@/assets/scss/variables";
 
 .main {
   display: grid;
